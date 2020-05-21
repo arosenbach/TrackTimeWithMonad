ff73162e4747bfa2c4df1761c2b5cfa4d2c04bc1
Antoine Rosenbach <arosenbach@users.noreply.github.com>
2020-05-21 19:45:12 +0200

    add random names to User (#15)

--- src/main/java/timedmonad/example/util/Random.java
+++ src/main/java/timedmonad/example/util/Random.java
@@ -1,7 +1,24 @@
 package timedmonad.example.util;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Random {
+
+    private final static List<String> FIRST_NAMES = Arrays.asList("Kellen", "Danna", "Aubree", "Jayvon", "Cecelia", "Piper",
+            "Cali", "Cali", "Rodney", "Tyshawn", "Alena", "Brayden", "Juan", "Van", "Yareli", "Raphael", "Timothy",
+            "Adonis", "Lilian", "Dakota", "Jakobe", "Reid", "Ross", "Alfredo", "Cora", "Shaun", "Johnny", "Brielle",
+            "Raphael", "Eden", "Selina", "Alison", "Nina", "Melissa", "Pierre");
+    private final static List<String> LAST_NAMES = Arrays.asList("Stephens", "Zamora", "Wilkerson", "Donovan", "Pruitt", "Hampton",
+            "Case", "Bridges", "Riggs", "Bright", "Guzman", "Brown", "Cooley", "Schwartz", "Krause", "Strong", "Gross", "Mccarty",
+            "Baker", "Khan", "Lynn", "Burgess", "Nielsen", "Sharp", "Conley", "Rasmussen", "Zimmerman", "Goodman", "Hopkins",
+            "Singleton", "Casey", "Everett", "Hanson", "Giles", "Petersen");
+
     public static int getRandomInt(int min, int max) {
         return min + (int) (Math.random() * ((max - min) + 1));
+    }
+
+    public static String getRandomName() {
+        return FIRST_NAMES.get(getRandomInt(0, FIRST_NAMES.size() - 1)) + " " + LAST_NAMES.get(getRandomInt(0, LAST_NAMES.size() - 1));
     }
 }
